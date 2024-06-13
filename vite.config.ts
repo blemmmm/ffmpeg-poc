e8a2d36c93c91abd7a38e8c734b4c529a4c58423
Blessly Pera <blessly.pera@media-meter.com>
2024-06-13 11:59:13 +0800

    add vite plugin for cors

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+function crossOriginIsolationMiddleware(_, response, next) {
+  response.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  response.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+}
+
+const crossOriginIsolation = () => {
+  return {
+    name: "cross-origin-isolation",
+    configureServer: (server) => {
+      server.middlewares.use(crossOriginIsolationMiddleware);
+    },
+    configurePreviewServer: (server) => {
+      server.middlewares.use(crossOriginIsolationMiddleware);
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crossOriginIsolation()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,6 +31,7 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
+
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
